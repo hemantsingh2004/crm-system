1e45312fae7d249a2d4869d812e4962b8d93262f
Hemant Singh <hemantsingh.github@gmail.com>
2025-01-27 17:42:32 +0530

    Add New Ticket Page completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import { DefaultLayout } from "./layout/DefaultLayout";
 import { Dashboard } from "./pages/dashboard/Dashboard.page";
 import { Entry } from "./pages/entry/Entry.page";
+import { AddTicket } from "./pages/new-ticket/AddTicket.page";
 import "./App.css";
 function App() {
   return(
     <div className="App">
       {/* <Entry/> */}
       <DefaultLayout>
-        <Dashboard/>
+        {/* <Dashboard/> */}
+        <AddTicket />
       </DefaultLayout>
     </div>
   )
