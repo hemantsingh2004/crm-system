8fbdb84cf8f169dbc5d3d92bf4f31ee03979a9c1
Hemant Singh <hemantsingh.github@gmail.com>
2025-01-31 11:30:17 +0530

    Added Ticket Listing Page

---
+++ frontend/src/pages/ticket-listing/TicketLists.page.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { SearchForm } from "../../components/search-form/SearchForm.comp";
+import { TicketTable } from "../../components/ticket-table/TicketTable.comp";
+import DummyTickets from "../../assets/data/dummy-tickets.json";
+
+export const TicketLists = () => {
+  const [str, setStr] = useState("");
+  const [dispTickets, setDispTickets] = useState([DummyTickets]);
+  
+  const handleOnChange = (e) => setStr(e.target.value);
+
+  const searchTicket = (str) => {
+    const displayTickets = DummyTickets.filter((row) =>
+      row.subject.toLowerCase().includes(str.toLowerCase())
+    );
+    setDispTickets(displayTickets);
+  };
+
+  useEffect(() => {
+    searchTicket(str);
+  }, [str]);
+
+  return (
+    <Container className="mt-5">
+      <Row>
+        <Col>
+          <Button variant="info">Add New Ticket</Button>
+        </Col>
+        <Col className="text-right">
+          <SearchForm handleOnChange={handleOnChange} str={str} />
+        </Col>
+      </Row>
+      <hr />
+      <Row className="mt-4">
+        <Col>
+          <TicketTable tickets={dispTickets} />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
