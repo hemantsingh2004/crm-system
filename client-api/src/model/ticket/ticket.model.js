d6be026ee0020d4ace5ea7f22ec7be681bf4c2f6
Hemant Singh <hemantsingh.github@gmail.com>
2025-02-14 12:11:38 +0530

    Get ticket API added

--- client-api/src/model/ticket/ticket.model.js
+++ client-api/src/model/ticket/ticket.model.js
@@ -13,4 +13,30 @@ const insertTicket = async (ticketObj) => {
   });
 };
 
-export { insertTicket };
+const getTickets = async (clientId) => {
+  return new Promise((resolve, reject) => {
+    try {
+      ticketModel
+        .find({ clientId })
+        .then((data) => resolve(data))
+        .catch((err) => reject(err));
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+const getTicketById = async (ticketId, clientId) => {
+  return new Promise((resolve, reject) => {
+    try {
+      ticketModel
+        .findOne({ _id: ticketId, clientId })
+        .then((data) => resolve(data))
+        .catch((err) => reject(err));
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+export { insertTicket, getTickets, getTicketById };
